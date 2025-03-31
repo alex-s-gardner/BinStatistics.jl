99510fe9d410855bb609648a7078ba9317ca8132
Alex Gardner <32276930+alex-s-gardner@users.noreply.github.com>
2025-03-31 09:56:56 -0700

    Update functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -6,8 +6,8 @@ Returns a DataFrame containing function values for binned variables of `df`.
 
 # Arguments
 - `axis_col`: binning axes column(s)
-- `axis_edges`: bin edges for `axis_col`s, as vector-like things or ranges.
-- `bin_col`: column variable(s) to be binned on.  multiple columns essentially form a multidimensional array.
+- `axis_edges`: bin edges for `axis_col`s, as vector or ranges.
+- `bin_col`: column variable(s) to be binned on.
 - `grp_function = [nrow]`: column independent function(s) to be applied at group level
 - `var_function = [mean]`: column dependent function(s) to be applied to each `bin_col` at group level.  
   If multiple, then each `var_function` corresponds to the `bin_col` at that index.
